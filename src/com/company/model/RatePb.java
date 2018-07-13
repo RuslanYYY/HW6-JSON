2163737d4cda33bfdd079bf2ee71ff85e5c5beb8
Ruslan Yuzefovych <lossantos2016@gmail.com>
2018-07-13 10:30:49 +0300

    1

---
+++ src/com/company/model/RatePb.java
@@ -0,0 +1,34 @@
+package com.company.model;
+
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+import java.util.Scanner;
+
+public class RatePb {
+
+
+    private String date;
+
+
+
+
+    @SerializedName("exchangeRate")
+    private List<ExchangeRate> myList;
+
+    public String getDate() {
+        return date;
+    }
+
+    public List<ExchangeRate> getMyList() {
+        return myList;
+    }
+
+    @Override
+    public String toString() {
+        return "RatePb{" +
+                "date='" + date + '\'' +
+                ", myList=" + myList +
+                '}';
+    }
+}
